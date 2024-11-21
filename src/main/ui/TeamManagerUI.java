9a58a9e130b78649ec8e567c58d514feaabd5725
mtanak10 <156983307+mtanak10@users.noreply.github.com>
2024-11-20 23:16:56 -0800

    updated the view team actionlistener

--- src/main/ui/TeamManagerUI.java
+++ src/main/ui/TeamManagerUI.java
@@ -5,12 +5,14 @@ import javax.swing.*;
 import model.*;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
+
 
 public class TeamManagerUI extends JFrame {
 
     private Team team;
+    private CardLayout cardLayOut;
+
 
     public static void main(String[] args) {
         TeamManagerUI  ui = new TeamManagerUI();
@@ -19,23 +21,26 @@ public class TeamManagerUI extends JFrame {
 
     public TeamManagerUI(){
         team = new Team();
+        cardLayOut = new CardLayout();
         setSize(600, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
-        JPanel mainMenuPanel = new JPanel();
-        mainMenuPanel.setLayout(new GridLayout(7, 1));
+        JPanel mainMenuPanel = new JPanel(cardLayOut);
+        mainMenuPanel.setLayout(new GridLayout(8, 1));
 
         
 
         JButton addPlayerButton = new JButton("Add a Player");
-        addPlayerButton.addActionListener(new AddPlayerActionListener());
+        addPlayerButton.addActionListener(new AddPlayerActionListener(team));
         mainMenuPanel.add(addPlayerButton);
         
         JButton removePlayerButton = new JButton("Remove a Player");
+        removePlayerButton.addActionListener(new RemovePlayerActionListener(team));
         mainMenuPanel.add(removePlayerButton);
 
         JButton viewTeamButton = new JButton("View the Team");
+        viewTeamButton.addActionListener(new ViewTeamActionListener(team));
         mainMenuPanel.add(viewTeamButton);
 
         JButton checkButton = new JButton("Check the Availability of a Player");
---
+++ src/main/ui/ViewTeamActionListener.java
@@ -0,0 +1,55 @@
+package ui;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import model.Player;
+import model.Team;
+
+public class ViewTeamActionListener implements ActionListener{
+
+    private JFrame viewTeamFrame;
+    private Team team;
+    
+
+    public ViewTeamActionListener(Team team){
+        viewTeamFrame = new JFrame("Add Player");
+        this.team = team;
+        viewTeamFrame.setSize(400, 300);
+        viewTeamFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        viewTeamFrame.setLayout(new GridLayout(7, 2));
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+
+        viewTeamFrame.getContentPane().removeAll();
+
+        JLabel teamDisplay = new JLabel("Squad");
+        viewTeamFrame.add(teamDisplay);
+
+         for (Player player : team.getTeam()) {
+            JLabel p = new JLabel("\tName: " + player.getName() + "   " + "Position: " + player.getPosition() + " Availability: " + player.isAvailable());
+            viewTeamFrame.add(p);
+         }
+
+        JButton exitButton = new JButton("Exit");
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                viewTeamFrame.dispose(); // Close the add player window
+            }
+        });
+        viewTeamFrame.add(exitButton);
+
+        viewTeamFrame.setVisible(true);
+        
+    }
+    
+}
